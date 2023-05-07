85fe10b4341071b8fbd74dd3905beac55464a8af
Marten Veldthuis <marten@veldthuis.com>
2023-05-07 13:16:10 +0200

    Hungry toggle

--- app/piccola/page.tsx
+++ app/piccola/page.tsx
@@ -3,13 +3,33 @@
 import { useState } from "react";
 import data from "./data.json";
 
+function getRandomIntInclusive(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
 export default function Home() {
-  const [filter, setFilter] = useState("")
-  const products = data.filter(product => (product.name.includes(filter) || product.description.includes(filter)));
+  const [filter, setFilter] = useState("");
+  const [hungry, setHungry] = useState(false);
+
+  let products = data.filter(product => (product.name.includes(filter) || product.description.includes(filter)));
+
+  if (hungry) {
+    const idx = getRandomIntInclusive(0, products.length - 1)
+    products = [products[idx]]
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <input type="text" placeholder="Grep" value={filter} onChange={e => setFilter(e.target.value)} />
+
+      <label>
+        <input type="checkbox" checked={hungry} onChange={e => setHungry(e.target.checked)} />
+        Ik heb NU honger
+      </label>
+
+
       <table className="border-2">
         <thead>
           <tr>
