4ef93484833b4aa393ee65a3041bdd8a86f6abd1
Marten Veldthuis <marten@veldthuis.com>
2023-05-07 14:57:42 +0200

    style homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main>
-      <Link href="/piccola">Piccola Roma Beilen</Link>
+    <main className='p-4'>
+      <header>Beilen</header>
+      <Link href="/piccola" className="block w-64 h-64 p-4 bg-amber-200 rounded-md font-bold">Piccola Roma Beilen</Link>
     </main>
   )
 }
